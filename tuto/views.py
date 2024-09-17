637ea6d05be04746a8250e77939fcfa45d57aab5
baptiste-mignan <baptiste.mignan@gmail.com>
2024-09-17 12:29:34 +0200

    fin TP2

--- tuto/views.py
+++ tuto/views.py
@@ -1,15 +1,13 @@
-import yaml.loader
 from .app import app
 from flask import render_template
-import yaml , os.path
+from .models import get_sample
 
 @app.route("/")
 def home():
-    return render_template("home.html", title= "Hello World!",
-                           names = ["Pierre", "Paul", "Corinne"],)
+    return render_template("home.html", title="My Books !", books=get_sample())
 
- # @app.route("/livres")
- # def livres():
- #     return render_template("livres.html", title= "Mes livres",
- #                            names = ["Pierre", "Paul", "Corinne"],
- #                            livres = yaml.loader(open(os.path.join(os.path.dirname(__file__), "data.yml"))))
+@app.route("/detail/<id>")
+def detail(id):
+    books = get_sample()
+    book = books[int(id)]
+    return render_template("detail.html", book=book)
