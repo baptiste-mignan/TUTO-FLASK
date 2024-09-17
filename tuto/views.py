86f8abff011c575e72245fd2a553216cba1a4bb0
baptiste-mignan <baptiste.mignan@gmail.com>
2024-09-17 11:23:51 +0200

    correction bug

--- tuto/views.py
+++ tuto/views.py
@@ -1,7 +1,15 @@
+import yaml.loader
 from .app import app
 from flask import render_template
+import yaml , os.path
 
 @app.route("/")
-
 def home():
-    return render_template("home.html", title= "Hello World!", names = ["Pierre", "Paul", "Corinne"])
+    return render_template("home.html", title= "Hello World!",
+                           names = ["Pierre", "Paul", "Corinne"],)
+
+ # @app.route("/livres")
+ # def livres():
+ #     return render_template("livres.html", title= "Mes livres",
+ #                            names = ["Pierre", "Paul", "Corinne"],
+ #                            livres = yaml.loader(open(os.path.join(os.path.dirname(__file__), "data.yml"))))
