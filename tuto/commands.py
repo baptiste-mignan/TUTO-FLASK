ee9730af11846c5f5301ddcd21b8acbecd719950
baptiste-mignan <baptiste.mignan@gmail.com>
2024-10-08 15:13:48 +0200

    ajout d'une commande pour changer le mot de passe

--- tuto/commands.py
+++ tuto/commands.py
@@ -1,5 +1,8 @@
 import click
 from .app import app, db
+from .models import User
+from hashlib import sha256
+
 @app.cli.command()
 @click.argument('filename')
 def loaddb(filename):
@@ -42,10 +45,18 @@ def syncdb():
 @click.argument('password')
 def newuser(username, password):
     '''Adds a new user.'''
-    from .models import User
-    from hashlib import sha256
     m = sha256()
     m.update(password.encode())
     u = User(username = username, password = m.hexdigest())
     db.session.add(u)
+    db.session.commit()
+
+@app.cli.command()
+@click.argument('username')
+@click.argument('password')
+def passwd(username, password):
+    m = sha256()
+    m.update(password.encode())
+    u = User.query.filter(User.username == username)[0]
+    u.password = m.hexdigest()
     db.session.commit()
