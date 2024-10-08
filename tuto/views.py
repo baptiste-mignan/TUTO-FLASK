60a57bb530f659d00f84e8c4d2272596c40c8628
baptiste-mignan <baptiste.mignan@gmail.com>
2024-10-08 11:47:32 +0200

    save author fonctionnel

--- tuto/views.py
+++ tuto/views.py
@@ -1,6 +1,11 @@
-from .app import app
-from flask import render_template
+from .app import app, db
+from flask import render_template, url_for , redirect
 from .models import get_sample
+from .models import Author
+from .models import get_author
+from flask_wtf import FlaskForm
+from wtforms import StringField , HiddenField
+from wtforms . validators import DataRequired
 
 @app.route("/")
 def home():
@@ -12,32 +17,25 @@ def detail(id):
     book = books[int(id)-1]
     return render_template("detail.html", book=book)
 
-from flask_wtf import FlaskForm
-from wtforms import StringField , HiddenField
-from wtforms . validators import DataRequired
 class AuthorForm(FlaskForm):
     id = HiddenField('id')
     name = StringField('Nom', validators = [DataRequired()])
 
-from .models import get_author
 @app.route("/edit/author/<int:id>")
 def edit_author(id):
     a = get_author(id)
     f = AuthorForm(id=a.id, name=a.name)
     return render_template("edit-author.html", author =a, form=f)
 
-from flask import url_for , redirect
-from .app import db
-from .models import Author
 @app.route("/save/author/", methods =("POST" ,))
 def save_author():
     a = None
     f = AuthorForm()
     if f.validate_on_submit():
         id = int(f.id.data)
-        a = get_author(id)
+        a = get_author(id)  
         a.name = f.name.data
-        db. session.commit()
-        return redirect(url_for('one_author', id=a.id))
+        db.session.commit()
+        return redirect(url_for('detail', id=a.id))
     a = get_author(int(f.id.data))
     return render_template("edit-author.html", author =a, form=f)
